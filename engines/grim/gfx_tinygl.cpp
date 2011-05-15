e94c5dec350863ef913d3c4684e965e0fc14e442
Joni Vähämäki <jonivah@gmail.com>
2011-05-15 18:47:37 +0300

    GRIM: Don't draw sprites when rendering shadows.

--- engines/grim/gfx_tinygl.cpp
+++ engines/grim/gfx_tinygl.cpp
@@ -537,14 +537,16 @@ void GfxTinyGL::drawHierachyNode(const Model::HierNode *node) {
 		tglPushMatrix();
 		tglTranslatef(node->_pivot.x(), node->_pivot.y(), node->_pivot.z());
 
-		Sprite* sprite = node->_sprite;
-		while (sprite) {
-			sprite->draw();
-			sprite = sprite->_next;
-		}
+		if (!_currentShadowArray) {
+			Sprite* sprite = node->_sprite;
+			while (sprite) {
+				sprite->draw();
+				sprite = sprite->_next;
+			}
 
-		if (node->_mesh && node->_meshVisible) {
-			node->_mesh->draw();
+			if (node->_mesh && node->_meshVisible) {
+				node->_mesh->draw();
+			}
 		}
 
 		tglMatrixMode(TGL_MODELVIEW);
